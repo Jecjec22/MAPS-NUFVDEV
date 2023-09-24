e812c668acb9c18161208e3c4be635f18f79e2d5
Jecjec22 <jecrodriguez3@gmail.com>
2023-09-24 14:12:15 +0800

    New

---
+++ employee/includes/footer.php
@@ -0,0 +1,5 @@
+ <div class="container-fluid">
+                     <div class="footer">
+                        <p>MANPOWER ALLOCATION AND PLANNING SYSTEM. NUFVDEV.</p>
+                     </div>
+                  </div>
